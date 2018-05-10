41614414ae6ab9141dacccd99e24edb7a07315bd
leiyuxin <617705033@qq.com>
2018-05-10 23:50:10 +0800

    增加Apache Common Lang 依赖 重写hashcode 与 equals方法 增加仓库接口
    Signed-off-by: leiyuxin <617705033@qq.com>

---
+++ springmvcproject1/src/test/java/spittr/web/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package spittr.web;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import spittr.web.HomeController;
+
+public class HomeControllerTest {
+
+  @Test
+  public void testHomePage() throws Exception {
+    HomeController controller = new HomeController();
+    MockMvc mockMvc = standaloneSetup(controller).build();
+    MockHttpServletRequestBuilder s = get("/home");
+    ResultMatcher ss = view().name("home");
+    System.out.println("sssss");
+  }
+
+}
